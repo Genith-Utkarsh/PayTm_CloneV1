e2de9a119939d4ad74baa6c88b09c7ee5b4b7294
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-26 16:37:15 +0530

    added sigin route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,53 +1,79 @@
-const express = require("express")
-const zod = require("zod")
-const User = require("../db/db")
-const jwt = require("jsonwebtoken")
-const secret = process.env.JWT_SECRET
-const router = express.Router()
-
-
-
+const express = require("express");
+const zod = require("zod");
+const User = require("../db/db");
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
+const router = express.Router();
 
 router.post("/signup", async (req, res) => {
+  const userInfo = req.body;
 
-    const userInfo = req.body
+  const schema = zod.object({
+    username: zod.string(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+  });
 
-    const schema = zod.object({
-        username : zod.string(),
-        password : zod.string(),
-        firstName : zod.string(),
-        lastName : zod.string(),
-    })
+  const responce = schema.safeParse(userInfo);
 
-    const responce = schema.safeParse(userInfo)
+  // zod validation
+  if (!responce) {
+    return res.send("Given inputs doesnot match correct format..");
+  }
 
-    // zod validation
-    if(!responce){
-        return res.send("Given inputs doesnot match correct format..")
+  const { username } = req.body;
+  // Checking if user exist or not in data base
+  const userExists = await User.findOne({ username });
+
+  if (userExists) {
+    return res
+      .status(409)
+      .send("User already exist ... Use different username..");
+  }
+
+  const payload = { username };
+  const token = jwt.sign(payload, secret);
+
+  try {
+    await User.create(userInfo);
+
+    res.status(200).json({
+      msg: "User created successfully..",
+      tokenId: token,
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(501)
+      .send("Error creating user.. somthing is up with our server");
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(411).send("User not found in our database");
     }
 
-    // Checking if user exist or not in data base
-    const userExists = await User.findOne({username})
-
-    if(userExists){
-        return res.status(409).send("User already exist ... Use different username..")
+    if (user.password !== password) {
+      return res.status(411).send("Wrong password");
     }
-    const {username} = req.body
-    const payload = {username}
-    const token = jwt.sign(payload, secret)
 
-    try {
-        await User.create(userInfo)
+    const payload = { username };
+    const token = jwt.sign(payload, secret);
 
-        res.status(200).json({
-            msg : "User created successfully..",
-            tokenId : token
-        })
-    } catch(err){
-        console.log(err)
-        res.status(501).send("Error creating user.. somthing is up with our server")
-    }
-})
+    res.status(200).json({
+      message: "Sign in successful",
+      tokenId: token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(501).send("Error while sign in..");
+  }
+});
 
-
-module.exports = router
+module.exports = router;
