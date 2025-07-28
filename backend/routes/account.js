400117b25ef23619be4f3ccc93f80e0822c5aa93
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-28 23:41:21 +0530

    done account section

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -23,4 +23,40 @@ router.get("/balance", authMiddlware, async (req, res) => {
 router.post("/transfer", authMiddlware ,async(req, res) => {
     const session = await mongoose.startSession()
     session.startTransaction()
+
+    const {amount , to} = req.body
+
+    const account = await Account.findOne({userId : req.userId}).session(session)
+    if(!account || account.balance < amount){
+        // abort the session
+        await session.abortTransaction()
+        return res.status(400).json({
+            message : "insuficient balance"
+        })
+    }
+
+
+    const toAccount = await Account.findOne({userId : to}).session(session)
+
+    if(!toAccount){
+        await session.abortTransaction()
+        return res.status(400).json({
+            message : "User account doesnot exist"
+        })
+    }
+
+
+    // Performing the trasaction by Updating the value
+    // Receiver =>  +amount              Sender =>   -amount
+
+    await Account.updateOne({userId : req.userId}, {$inc : {balance : -amount}}).session(session)
+    await Account.updateOne({userId : to}, {$inc : {balance : +amount}}).session(session)
+
+
+    // commiting the transaction
+    await session.commitTransaction()
+
+    res.status(200).json({
+        message : "Transaction successfull"
+    })
 })
