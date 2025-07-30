38a33cf915bdb2b9036ff0b2aaf9bf2592590af4
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-30 16:20:02 +0530

    structured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+i;
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/signup"/>
-            <Route path='/signin' />
-            <Route path='/dashboard'  />
-            <Route path='/send'    />
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" />
+          <Route path="/signin" />
+          <Route path="/dashboard" />
+          <Route path="/send" />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
