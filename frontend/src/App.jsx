156435f6880449703e3fca3df1bb2e54b8637af8
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-30 16:10:40 +0530

    Started building frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -8,7 +9,14 @@ function App() {
 
   return (
     <>
-      
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signup"/>
+            <Route path='/signin' />
+            <Route path='/dashboard'  />
+            <Route path='/send'    />
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
